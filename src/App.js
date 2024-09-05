9164276d2a332264e4f4fda2af18e39116be92dd
Rohit Manohar <manoharrohit372@gmail.com>
2024-09-05 22:30:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Components/Header';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Portfolio from './Components/Portfolio';
-import Contact from './Components/Contact';
-import Blog from './Components/Blog';
-import './index.css'; // Import your CSS file
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Portfolio from "./Components/Portfolio";
+import Contact from "./Components/Contact";
+import Blog from "./Components/Blog";
+import "./index.css"; // Import your CSS file
 
 function App() {
   return (
     <Router>
+      <div>
       <Header />
-      <div className="lines">
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
       </div>
+      
+      
       <main>
         <Routes>
           <Route path="/" element={<About />} />
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,93 @@
-import React from 'react';
+import React from "react";
+import { useRef } from "react";
 
 const About = () => {
-  return (
-    <div className='min-h-screen bg-black'>
+  const lettersRef = useRef([]);
 
+  return (
+    <div className="relative z-10">
+      {/* Background animation */}
+      <div className="absolute inset-0">
+        <div className="area">
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Content */}
+      <div className="relative z-20">
+        <div className="behind-content">
+          <div className="text-overlay">
+            <div className="font-acme text-9xl mt-96 text-violet-600">
+              <div>
+                {"Hi There!".split("").map((char, index) => (
+                  <span
+                    key={index}
+                    ref={(el) => (lettersRef.current[index] = el)}
+                    className="inline-block" // inline-block prevents line breaks
+                  >
+                    {char === " " ? "\u00A0" : char}{" "}
+                    {/* Non-breaking space for spaces */}
+                  </span>
+                ))}
+              </div>
+              <div className="font-bold text-3xl text-amber-400">
+                I am into WebDev
+              </div>
+              <p className="text-white text-base font-roboto mt-7 bg-purple-800/50 p-4 rounded-lg shadow-md">
+                I am a Computer Science student at NITK with a passion for
+                coding and problem-solving. I enjoy learning new technologies
+                and tackling challenging projects in web development and
+                programming. Outside of coding, I explore tech trends and
+                participate in competitions. I'm eager to collaborate with
+                others and contribute to the tech community.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mockup mockup-macbook loaded opened ml-36 mt-9">
+          <div className="part top">
+            <img
+              src="https://d1xm195wioio0k.cloudfront.net/images/mockup/macbook-top.svg"
+              alt=""
+              className="top"
+            />
+            <img
+              src="https://d1xm195wioio0k.cloudfront.net/images/mockup/macbook-cover.svg"
+              alt=""
+              className="cover"
+            />
+            <img
+              src="https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_5XySWqeTsJYccXgiU6pbTXmjkpih%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_69W1g56UQ3afqs1J92SU1Cogwcak"
+              alt="Laptop Screen"
+              className="screen-image"
+            />
+          </div>
+          <div className="part bottom">
+            <img
+              src="https://d1xm195wioio0k.cloudfront.net/images/mockup/macbook-cover.svg"
+              alt=""
+              className="cover"
+            />
+            <img
+              src="https://d1xm195wioio0k.cloudfront.net/images/mockup/macbook-bottom.svg"
+              alt=""
+              className="bottom"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
